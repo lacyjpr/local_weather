ce4c5a56fc90c3d998bdccc49f3b03fbeb873c40
Jean-Paul Lacy <lacyjpr@gmail.com>
2016-09-26 15:56:40 -0700

    add openweather api

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
-	$.getJSON("https://freegeoip.net/json?callback=?", function(data) {
-		console.log(data);
+
+	var lat = 0;
+	var lon = 0;
+
+	function getLocation() {
+		$.getJSON("http://freegeoip.net/json?callback=?", function(data) {
+			console.log(data);
+			lat = data.latitude;
+			console.log("lat " + lat);
+			lon = data.longitude;
+			console.log("lon " + lon);
+		});
+	}
+	getLocation();
+
+	$.when(getLocation()).done(function(){
+		// Use cors-anywhere to avoid mixed content message
+		$.getJSON("https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=d5cc4a4def8bbe77201724558d5bd532", function(response) {
+			console.log(response);
+		}); 
+
 	});
 });
+
+//openweather key d5cc4a4def8bbe77201724558d5bd532
