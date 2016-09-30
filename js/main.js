2bfb2c6943280c2be0c0ca63495cb60ead71d4d0
Jean-Paul Lacy <lacyjpr@gmail.com>
2016-09-29 18:06:18 -0700

    Fix comments

--- js/main.js
+++ js/main.js
@@ -11,13 +11,19 @@ function getWeather() {
 			var apiKey = "d5cc4a4def8bbe77201724558d5bd532";
 			// Use cors-anywhere to avoid "mixed content" errors
 			var url = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?units=imperial&lat=" + latitude + "&lon=" + longitude +   "&appid=" + apiKey;
-			// Chain 2nd ajax within success so it can access the latitude & longitude variables Credit GMunker for showing me how this works http://codepen.io/Gmunker/pen/QKrgkz?editors=0012
+			// Chain 2nd ajax within success so it can access the latitude & longitude variables. Credit GMunker for showing me how this works http://codepen.io/Gmunker/pen/QKrgkz?editors=0012
 			$.ajax({
 				url: url,
 				success: function(data) {
 					console.log(data);
+				},
+				error: function() {
+					console.log("Weather not found");
 				} 
 			});
+		},
+		error: function() {
+			console.log("Location not found");
 		}
 	});
 }
