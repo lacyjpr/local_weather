ce2a8c8a075da8b3e57fffef94bef90be20d3c7f
Jean-Paul Lacy <lacyjpr@gmail.com>
2016-09-29 22:29:14 -0700

    Add C/F toggle

--- js/main.js
+++ js/main.js
@@ -16,10 +16,17 @@ function getWeather() {
 				url: url,
 				success: function(data) {
 					console.log(data);
+
 					$("#city").html(data.name + ", " + data.sys.country);
-					$("#temp").html(Math.round(data.main.temp) + "&deg;F");
+					var cel = (5/9) * (data.main.temp - 32);
+					$("#tempCel").html(Math.round(cel) + "&deg;C").hide();
+					$("#tempFah").html(Math.round(data.main.temp) + "&deg;F");
 					$("#desc").html(data.weather[0].description);
 					$("#icon").html("<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png' alt='current weather icon'>");
+					$("#toggle").click(function(){
+						$("#tempCel").toggle();
+						$("#tempFah").toggle();
+					});
 				},
 				error: function() {
 					console.log("Weather not found");
